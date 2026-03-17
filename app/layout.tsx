d6deb78427444c4bbfe9f7797e693a4d08101f09
SuyashC1000 <107310104+SuyashC1000@users.noreply.github.com>
2026-03-17 23:26:56 +0530

    feat: initialize repository

---
+++ app/layout.tsx
@@ -0,0 +1,60 @@
+import type { Metadata } from "next";
+import localFont from "next/font/local";
+import "./globals.css";
+
+const manrope = localFont({
+  src: [
+    {
+      path: "../public/fonts/manrope-thin.otf",
+      weight: "100",
+      style: "normal",
+    },
+    {
+      path: "../public/fonts/manrope-light.otf",
+      weight: "300",
+      style: "normal",
+    },
+    {
+      path: "../public/fonts/manrope-regular.otf",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "../public/fonts/manrope-medium.otf",
+      weight: "500",
+      style: "normal",
+    },
+    {
+      path: "../public/fonts/manrope-semibold.otf",
+      weight: "600",
+      style: "normal",
+    },
+    {
+      path: "../public/fonts/manrope-bold.otf",
+      weight: "700",
+      style: "normal",
+    },
+    {
+      path: "../public/fonts/manrope-extrabold.otf",
+      weight: "800",
+      style: "normal",
+    },
+  ],
+});
+
+export const metadata: Metadata = {
+  title: "SE Project",
+  description: "Software Engineering Project",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${manrope.className} antialiased`}>{children}</body>
+    </html>
+  );
+}
