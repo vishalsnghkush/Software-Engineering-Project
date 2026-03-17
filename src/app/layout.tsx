f29ff2de1d846ef0645273c250e4047b6d25f0b1
SuyashC1000 <107310104+SuyashC1000@users.noreply.github.com>
2026-03-18 00:23:02 +0530

    chore: dockerize nextjs app

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,37 +5,37 @@ import "./globals.css";
 const manrope = localFont({
   src: [
     {
-      path: "../public/fonts/manrope-thin.otf",
+      path: "../../public/fonts/manrope-thin.otf",
       weight: "100",
       style: "normal",
     },
     {
-      path: "../public/fonts/manrope-light.otf",
+      path: "../../public/fonts/manrope-light.otf",
       weight: "300",
       style: "normal",
     },
     {
-      path: "../public/fonts/manrope-regular.otf",
+      path: "../../public/fonts/manrope-regular.otf",
       weight: "400",
       style: "normal",
     },
     {
-      path: "../public/fonts/manrope-medium.otf",
+      path: "../../public/fonts/manrope-medium.otf",
       weight: "500",
       style: "normal",
     },
     {
-      path: "../public/fonts/manrope-semibold.otf",
+      path: "../../public/fonts/manrope-semibold.otf",
       weight: "600",
       style: "normal",
     },
     {
-      path: "../public/fonts/manrope-bold.otf",
+      path: "../../public/fonts/manrope-bold.otf",
       weight: "700",
       style: "normal",
     },
     {
-      path: "../public/fonts/manrope-extrabold.otf",
+      path: "../../public/fonts/manrope-extrabold.otf",
       weight: "800",
       style: "normal",
     },
