b7cf4509acb5f80e64039b5284240223c84b4e95
keshavkhandelwal001 <keshavkhandelwal151@ce.du.ac.in>
2026-03-18 08:48:02 +0530

    Add new fields to usersTable schema

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,8 +1,17 @@
-import { integer, pgTable, varchar } from "drizzle-orm/pg-core";
+import { integer, pgTable, varchar, timestamp } from "drizzle-orm/pg-core";
 
 export const usersTable = pgTable("users", {
   id: integer().primaryKey().generatedAlwaysAsIdentity(),
+
   name: varchar({ length: 255 }).notNull(),
-  age: integer().notNull(),
+
   email: varchar({ length: 255 }).notNull().unique(),
+
+  password: varchar({ length: 255 }).notNull(),
+
+  role: varchar({ length: 50 }).notNull().default("student"), 
+
+  createdAt: timestamp().defaultNow().notNull(),
+
+  updatedAt: timestamp().defaultNow().notNull(),
 });
