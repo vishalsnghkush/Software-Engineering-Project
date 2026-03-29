ed7c1eb65496416ab2ee4a0005e2423a819e47ce
SuyashC1000 <107310104+SuyashC1000@users.noreply.github.com>
2026-03-29 21:12:52 +0530

    feat: add test sidebar legend for mobile

--- src/app/(main)/home/page.tsx
+++ src/app/(main)/home/page.tsx
@@ -1,12 +1,17 @@
 "use client";
 
 import { Button } from "@heroui/react";
+import Link from "next/link";
 import React from "react";
 
 const HomePage = () => {
   return (
     <div>
       <h1 className="font-bold text-2xl">Home</h1>
+
+      <Link href={"/test/0"}>
+        <Button>Test</Button>
+      </Link>
     </div>
   );
 };
--- src/app/test/[slug]/_components/test-sidebar.tsx
+++ src/app/test/[slug]/_components/test-sidebar.tsx
@@ -1,6 +1,33 @@
-import { Button } from "@heroui/react";
+import { Button, Popover, PopoverContent, PopoverTrigger } from "@heroui/react";
 import { InfoIcon } from "@phosphor-icons/react";
 import React from "react";
+
+const TestSidebarLegend = () => {
+  return (
+    <div className="flex flex-col gap-2">
+      <div className="flex gap-2 items-center">
+        <Button isIconOnly>1</Button>
+        <p className="text-sm">Not Visited</p>
+      </div>
+      <div className="flex gap-2 items-center">
+        <Button isIconOnly>1</Button>
+        <p className="text-sm">Answered</p>
+      </div>
+      <div className="flex gap-2 items-center">
+        <Button isIconOnly>1</Button>
+        <p className="text-sm">Not Answered</p>
+      </div>
+      <div className="flex gap-2 items-center">
+        <Button isIconOnly>1</Button>
+        <p className="text-sm">Marked for Review</p>
+      </div>
+      <div className="flex gap-2 items-center">
+        <Button isIconOnly>1</Button>
+        <p className="text-sm">Answered & Marked for Review</p>
+      </div>
+    </div>
+  );
+};
 
 const TestSidebar = () => {
   return (
@@ -13,30 +40,21 @@ const TestSidebar = () => {
         ))}
       </div>
       <div className="ml-auto md:ml-0">
-        <Button isIconOnly className="md:hidden">
-          <InfoIcon size={20} />
-        </Button>
-        <div className="hidden md:flex flex-col gap-2">
-          <div className="flex gap-2 items-center">
-            <Button isIconOnly>1</Button>
-            <p className="text-sm">Not Visited</p>
-          </div>
-          <div className="flex gap-2 items-center">
-            <Button isIconOnly>1</Button>
-            <p className="text-sm">Answered</p>
-          </div>
-          <div className="flex gap-2 items-center">
-            <Button isIconOnly>1</Button>
-            <p className="text-sm">Not Answered</p>
-          </div>
-          <div className="flex gap-2 items-center">
-            <Button isIconOnly>1</Button>
-            <p className="text-sm">Marked for Review</p>
-          </div>
-          <div className="flex gap-2 items-center">
-            <Button isIconOnly>1</Button>
-            <p className="text-sm">Answered & Marked for Review</p>
-          </div>
+        <Popover placement="bottom-end">
+          <PopoverTrigger>
+            <Button isIconOnly className="md:hidden">
+              <InfoIcon size={20} />
+            </Button>
+          </PopoverTrigger>
+          <PopoverContent>
+            <div className="p-2">
+              <TestSidebarLegend />
+            </div>
+          </PopoverContent>
+        </Popover>
+
+        <div className="hidden md:block">
+          <TestSidebarLegend />
         </div>
       </div>
     </div>
